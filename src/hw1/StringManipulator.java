bba4043b0c2334da416ca7f26530784a779b012c
JL <JL@JL>
2015-02-23 19:09:15 -0500

    Finish Stringmanipulator

--- src/hw1/StringManipulator.java
+++ src/hw1/StringManipulator.java
@@ -1,9 +1,9 @@
 package hw1;
-
+import java.util.Scanner;
 /**
  * Homework:String Manipulator.
  *
- * @author <a href="mailto:youremail@mail.citytech.cuny.edu">Your Name</a>.
+ * @author <a href="mailto:Jianlang.lin@mail.citytech.cuny.edu">Jianlang lin</a>.
  */
 public class StringManipulator {
 
@@ -20,5 +20,24 @@ public class StringManipulator {
          The name of the city in all lowercase letters
          The first character in the name of the city
          */
+        String input;
+        Scanner keyboard=new Scanner(System.in);
+        System.out.print("enter the name of your favorite city:");
+        input=keyboard.nextLine();
+        int leng=input.length();
+        String upper=input.toUpperCase();
+        String lower=input.toLowerCase();
+        char letter=input.charAt(0);
+        System.out.println(leng);
+        System.out.println(upper);
+        System.out.println(lower);
+        System.out.println(letter);
+        
+        
+        
+        
+        
+        
+                
     }
 }
