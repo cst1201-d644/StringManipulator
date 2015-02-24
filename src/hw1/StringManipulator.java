7ef0236185a8ef3888a963ec5ac0d34e8bf11ac1
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-02-23 19:02:17 -0500

    Initial files for Homework: String Manipulator.

---
+++ src/hw1/StringManipulator.java
@@ -0,0 +1,24 @@
+package hw1;
+
+/**
+ * Homework:String Manipulator.
+ *
+ * @author <a href="mailto:youremail@mail.citytech.cuny.edu">Your Name</a>.
+ */
+public class StringManipulator {
+
+    /**
+     * The main method. This is the method that will execute when you start your
+     * program.
+     */
+    public static void main(String[] args) {
+        /*
+         Write a program that asks the user to enter the name of his or her favorite city. 
+         Use a String variable to store the input. The program should display the following:
+         The number of characters in the city name
+         The name of the city in all uppercase letters
+         The name of the city in all lowercase letters
+         The first character in the name of the city
+         */
+    }
+}
