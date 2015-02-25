e03ae295876b926f4fa10f905a14ce30f3cc6225
15332071 <15332071@CST_N417_15.cst.citytech.cuny.edu>
2015-02-25 17:12:41 -0500

    This is update HomeWork GKundu

--- src/hw1/StringManipulator.java
+++ src/hw1/StringManipulator.java
@@ -1,24 +1,37 @@
 package hw1;
-
+import java.util.Scanner;
 /**
  * Homework:String Manipulator.
  *
- * @author <a href="mailto:youremail@mail.citytech.cuny.edu">Your Name</a>.
+ * @author <a href="mailto:goutam.kundu@mail.citytech.cuny.edu">Goutam Kundu</a>.
  */
 public class StringManipulator {
 
     /**
      * The main method. This is the method that will execute when you start your
      * program.
+     * @param args
      */
     public static void main(String[] args) {
-        /*
-         Write a program that asks the user to enter the name of his or her favorite city. 
+        
+         /*Write a program that asks the user to enter the name of his or her favorite city. 
          Use a String variable to store the input. The program should display the following:
          The number of characters in the city name
          The name of the city in all uppercase letters
          The name of the city in all lowercase letters
-         The first character in the name of the city
-         */
+         The first character in the name of the city*/
+         System.out.println("Please enter your favorite city: ");
+         Scanner sc = new Scanner(System.in);
+
+        String in = sc.nextLine();
+        String Upper = in.toUpperCase();// all uppercase
+        String Lower = in.toLowerCase();// all Lowercase
+        int size = in.length();// Number of characters
+        char inital = Upper.charAt(0);// First Characters
+
+        System.out.println(size);
+        System.out.println(Upper);
+        System.out.println(Lower);
+        System.out.println(inital);
     }
 }
