3a3d16f513a48e9ef30e24b20746c75e1cdfb47c
class <class@CST_N417_19.cst.citytech.cuny.edu>
2015-02-25 17:08:17 -0500

    StringManipulator

--- String Manipulator/src/string/manipulator/StringManipulator.java
+++ String Manipulator/src/string/manipulator/StringManipulator.java
@@ -21,24 +21,18 @@ public class StringManipulator {
         // Create a Scanner object for keyboard input.
     Scanner keyboard = new Scanner(System.in);
 
-    // Get the user's favorite city.
     System.out.print("Enter the name of your favorite city: ");
     
     String city = keyboard.nextLine();
 
-    // close stream
     keyboard.close();
-    
-    // Display the number of characters.
+
     System.out.println("Number of characters: " + city.length());
 
-    // Display the city name in uppercase characters.
     System.out.println(city.toUpperCase());
 
-    // Display the city name in lowercase characters.
     System.out.println(city.toLowerCase());
 
-    // Display the first character in the city name.
     System.out.println(city.charAt(0));
     }
     
