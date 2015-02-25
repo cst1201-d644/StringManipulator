4579c65ee32e67e8bc4d4d924511b79a5f302919
Cissco <Cissco@ciisco8>
2015-02-25 01:34:23 -0500

    Homework1_Done

--- src/hw1/StringManipulator.java
+++ src/hw1/StringManipulator.java
@@ -1,5 +1,7 @@
 package hw1;
 
+import java.util.Scanner;
+
 /**
  * Homework:String Manipulator.
  *
@@ -10,6 +12,7 @@ public class StringManipulator {
     /**
      * The main method. This is the method that will execute when you start your
      * program.
+     * @param args
      */
     public static void main(String[] args) {
         /*
@@ -20,5 +23,21 @@ public class StringManipulator {
          The name of the city in all lowercase letters
          The first character in the name of the city
          */
+             System.out.println("Please enter your favorite city: ");
+
+Scanner scan = new Scanner(System.in);
+
+String input = scan.nextLine();
+String upper = input.toUpperCase();// all uppercase
+String lower = input.toLowerCase();// all Lowercase
+int size = input.length();// Number of characters
+char inital = upper.charAt(0);// First Character
+
+
+
+System.out.println(size);
+System.out.println(upper);
+System.out.println(lower);
+System.out.println(inital);
     }
 }
