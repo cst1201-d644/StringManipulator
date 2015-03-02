98c86420f4302ba9cf79bfe3582c610000ba3ffc
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-03-02 17:47:12 -0500

    Copied code from Homework into StringManipulator.

--- src/hw1/StringManipulator.java
+++ src/hw1/StringManipulator.java
@@ -20,5 +20,27 @@ public class StringManipulator {
          The name of the city in all lowercase letters
          The first character in the name of the city
          */
+
+        String city;
+        // Ask the user's favorite city.
+        try ( //  Create a Scanner object for keyboard input.
+                Scanner keyboard = new Scanner(System.in)) {
+            // Ask the user's favorite city.
+            System.out.println("Enter the name of your city: ");
+            city = keyboard.nextLine();
+            // close stream
+        }
+
+        // Show the number of characters.
+        System.out.println("Number of characters: " + city.length());
+
+        //Show the city name in uppercase characters.
+        System.out.println(city.toUpperCase());
+
+            // Show the city name in lowercase characters.
+        System.out.println(city.toLowerCase());
+
+        // Show the first character in the city name.
+        System.out.println(city.charAt(0));
     }
 }
